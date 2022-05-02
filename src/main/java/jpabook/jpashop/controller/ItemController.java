b5f921db0edf4752ddc017fc06ffa11717ce125a
slrslrr2 <slrslrr1@gmail.com>
2022-05-02 21:59:59 +0900

    웹 계층 개발 - 변경감지와 병합, 상품주문

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -62,17 +62,18 @@ public class ItemController {
     }
 
     @PostMapping("items/{itemId}/edit")
-    public String updateItemForm(@PathVariable String itemId, @ModelAttribute("Form") BookForm form){
-        Book book = new Book();
+    public String updateItemForm(@PathVariable Long itemId, @ModelAttribute("Form") BookForm form){
+//        Book book = new Book();
+//        book.setId(form.getId());
+//        book.setName(form.getName());
+//        book.setPrice(form.getPrice());
+//        book.setStockQuantity(form.getStockQuantity());
+//        book.setAuthor(form.getAuthor());
+//        book.setIsbn(form.getIsbn());
+//
+//        itemService.saveItem(book);
 
-        book.setId(form.getId());
-        book.setName(form.getName());
-        book.setPrice(form.getPrice());
-        book.setStockQuantity(form.getStockQuantity());
-        book.setAuthor(form.getAuthor());
-        book.setIsbn(form.getIsbn());
-
-        itemService.saveItem(book);
+        itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
         return "redirect:items";
     }
 }
--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,7 +19,25 @@ public class ItemService {
     public void saveItem(Item item){
         itemRepository.save(item);
     }
-    
+
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity){
+        /**
+         1. itemRepository.findOne는 em.find로 가져온것이기때문에 영속성컨텍스트에 영속되어있다.
+         2. set으로 변경하였다면
+         3. @Transactional에 의하여 commit 이 될 것이고
+         4. flush가 발생하여
+                영속성컨텍스트에 1차캐시안에 엔티티와 스냅샷을 비교하여
+                변경된 점을 감지하여 [쓰기 지연 SQL 저장소]에 쿼리를 저장한 후,
+                SQL문을 DB에 보낸다.
+                이대로 변경됨!
+         */
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setPrice(price);
+        findItem.setName(name);
+        findItem.setStockQuantity(stockQuantity);
+    }
+
     public Item findOne(Long id){
         return itemRepository.findOne(id);
     }
